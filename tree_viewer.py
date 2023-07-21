f903b70e046225825be2762a843e507c22f27661
github-1970 <github1970@gmail.com>
2023-07-21 23:32:36 +0330

    Add tree viewer for zip file

--- tree_viewer.py
+++ tree_viewer.py
@@ -1,29 +1,37 @@
 import os
 import argparse
+import zipfile
+import tempfile
+import shutil
 
 def display_tree(directory_path, indent=''):
-    if not os.path.isdir(directory_path):
-        print('This path is not a directory!')
-        return
+    if os.path.isdir(directory_path):
+        files = os.listdir(directory_path)
+        files.sort()
 
-    files = os.listdir(directory_path)
-    files.sort()
-
-    for file in files:
-        file_path = os.path.join(directory_path, file)
-        if os.path.isfile(file_path):
-            print(indent + '- ' + file)
-        elif os.path.isdir(file_path):
-            print(indent + '|- ' + file)
-            display_tree(file_path, indent + '|  ')
+        for file in files:
+            file_path = os.path.join(directory_path, file)
+            if os.path.isfile(file_path):
+                print(indent + '- ' + file)
+            elif os.path.isdir(file_path):
+                print(indent + '|- ' + file)
+                display_tree(file_path, indent + '|  ')
+    elif zipfile.is_zipfile(directory_path):
+        with zipfile.ZipFile(directory_path, 'r') as zip_file:
+            temp_dir = tempfile.mkdtemp()
+            try:
+                zip_file.extractall(temp_dir)
+                display_tree(temp_dir)
+            finally:
+                shutil.rmtree(temp_dir)
 
 def main():
     parser = argparse.ArgumentParser(description="Display the directory tree.")
-    parser.add_argument("directory", help="Path to the target directory")
+    parser.add_argument("path", help="Path to the target directory or zip file")
     args = parser.parse_args()
 
-    target_directory = args.directory
-    display_tree(target_directory)
+    target_path = args.path
+    display_tree(target_path)
 
 if __name__ == "__main__":
     main()
